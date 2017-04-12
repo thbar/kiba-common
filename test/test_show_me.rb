26820a344a21566cea086336e6a0f18d5804f8a6
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-04-12 15:46:52 +0200

    Update test to use test helper

--- test/test_show_me.rb
+++ test/test_show_me.rb
@@ -1,21 +1,19 @@
 require_relative 'helper'
 require 'kiba'
 require 'kiba-common/dsl_extensions/show_me'
-require_relative 'support/test_enumerable_source'
 
 class TestShowMe < Minitest::Test
+  include AssertCalled
+
   def test_show_me
     job = Kiba.parse do
       extend Kiba::Common::DSLExtensions::ShowMe
       source TestEnumerableSource, ['row']
       show_me!
     end
-
-    mock = MiniTest::Mock.new
-    mock.expect(:call, nil, ['row'])
-    Kernel.stub(:ap, mock) do
+    
+    assert_called(Kernel, :ap, ['row']) do
       Kiba.run(job)
     end
-    mock.verify
   end
 end
