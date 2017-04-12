5be077b245eae226269911a6f543615633c76283
Thibaut Barrère <thibaut.barrere@gmail.com>
2017-04-12 15:37:58 +0200

    Add ShowMe extension

---
+++ lib/kiba-common/dsl_extensions/show_me.rb
@@ -0,0 +1,19 @@
+require 'ap'
+
+module Kiba
+  module Common
+    module DSLExtensions
+      # Color print your row.
+      module ShowMe
+        def show_me!
+          transform do |row|
+            # NOTE: invoking Kernel.ap for testing since
+            # ap itself is harder to mock (Kiba Context)
+            Kernel.ap(row)
+            row
+          end
+        end
+      end
+    end
+  end
+end
