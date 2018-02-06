f092553f12f7e0d2324b717cd63b365ae2784ea5
Thibaut Barrère <thibaut.barrere@gmail.com>
2018-02-06 09:44:38 +0100

    Provide a way to dynamically instantiate sources based on data rows

---
+++ test/test_source_transform_adapter.rb
@@ -0,0 +1,24 @@
+require_relative 'helper'
+require 'kiba-common/transforms/source_transform_adapter'
+
+class TestSourceTransformAdapter < Minitest::Test
+  include Kiba::Common::Sources
+  include Kiba::DSLExtensions
+
+  def test_instantiation
+    rows = []
+    job = Kiba.parse do
+      extend Config
+      config :kiba, runner: Kiba::StreamingRunner
+
+      source Enumerable, [
+        [ Enumerable, (1..10) ],
+        [ Enumerable, (11..20) ]
+      ]
+      transform SourceTransformAdapter
+      destination TestArrayDestination, rows
+    end
+    Kiba.run(job)
+    assert_equal (1..20).to_a, rows
+  end
+end
