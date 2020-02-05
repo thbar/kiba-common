d044269a4ac6529793a675aaaea2bb602e3ab93a
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-02-05 17:18:02 +0100

    Add extra tests for keyword scenarios

--- test/test_source_transform_adapter.rb
+++ test/test_source_transform_adapter.rb
@@ -1,5 +1,7 @@
 require_relative 'helper'
 require 'kiba-common/transforms/source_transform_adapter'
+require_relative 'support/test_keyword_proxy_source'
+require_relative 'support/test_hash_configured_object'
 
 class TestSourceTransformAdapter < Minitest::Test
   include Kiba::Common::Sources
@@ -20,5 +22,37 @@ class TestSourceTransformAdapter < Minitest::Test
     end
     Kiba.run(job)
     assert_equal (1..20).to_a, rows
+  end
+  
+  def test_instantiation_keyword_arguments
+    rows = []
+    job = Kiba.parse do
+      source Enumerable, [
+        # Test against a class that expects explicit keyword arguments
+        [ TestKeywordProxySource, {mandatory: "some value"} ]
+      ]
+      transform Kiba::Common::Transforms::SourceTransformAdapter
+      destination TestArrayDestination, rows
+    end
+    Kiba.run(job)
+    assert_equal([
+      {mandatory: "some value", optional: nil}
+    ], rows)
+  end
+  
+  def test_hash_configured_object
+    rows = []
+    job = Kiba.parse do
+      source Enumerable, [
+        # Test against a class that takes a single Hash argument
+        [ TestHashConfiguredObject, {mandatory: "some value"} ]
+      ]
+      transform Kiba::Common::Transforms::SourceTransformAdapter
+      destination TestArrayDestination, rows
+    end
+    Kiba.run(job)
+    assert_equal([
+      {mandatory: "some value"}
+    ], rows)
   end
 end
