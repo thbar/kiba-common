ba07233cb81e1daf95f1a304903611122b715339
wuarmin <armin@wurweb.com>
2020-04-22 13:28:37 +0200

    NEW: add row_pre_processor-feature to CSV-destination

--- lib/kiba-common/destinations/csv.rb
+++ lib/kiba-common/destinations/csv.rb
@@ -5,20 +5,28 @@ module Kiba
     module Destinations
       class CSV
         attr_reader :filename, :csv_options, :csv, :headers
-        
-        def initialize(filename:, csv_options: nil, headers: nil)
+
+        def initialize(filename:, csv_options: nil, headers: nil, row_pre_processor: nil)
           @filename = filename
           @csv_options = csv_options || {}
           @headers = headers
+          @row_pre_processor = row_pre_processor
         end
-        
+
         def write(row)
           @csv ||= ::CSV.open(filename, 'wb', csv_options)
-          @headers ||= row.keys
-          @headers_written ||= (csv << headers ; true)
-          csv << row.fetch_values(*@headers)
+          call_row_pre_processor(row).each do |row|
+            @headers ||= row.keys
+            @headers_written ||= (csv << headers ; true)
+            csv << row.fetch_values(*@headers)
+          end
         end
-        
+
+        def call_row_pre_processor(row)
+          return [row] if @row_pre_processor.nil?
+          [@row_pre_processor.call(row)].flatten.compact
+        end
+
         def close
           csv&.close
         end
